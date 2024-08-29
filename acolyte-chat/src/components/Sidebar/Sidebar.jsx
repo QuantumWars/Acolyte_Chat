144e375e2a87a06a60427c9cd46be7a95411c0db
QuantumWars <155221315+QuantumWars@users.noreply.github.com>
2024-08-29 21:55:03 +0530

    Update Sidebar.jsx

--- acolyte-chat/src/components/Sidebar/Sidebar.jsx
+++ acolyte-chat/src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import './Sidebar.css';
 import { Menu, Plus, CircleHelp, Settings, History, Database } from 'lucide-react';
-import { Context } from "../../context/context";
+import { Context } from "../../context/Context.jsx";
 
 const Sidebar = () => {
     const [extended, setExtended] = useState(false);
