d3d184ae53446fcc2171a8bc12cff3e3fc7da314
QuantumWars <155221315+QuantumWars@users.noreply.github.com>
2024-09-03 11:39:00 +0530

    Update Main.jsx

--- acolyte-chat/src/components/Main/Main.jsx
+++ acolyte-chat/src/components/Main/Main.jsx
@@ -57,7 +57,18 @@ const Main = () => {
             { text: "How did orexin receptor antagonism affect mice's preference for wheel running versus high-palatable food?", icon: Microscope },
             { text: "What role do hypocretin/orexin neurons (HONs) play in arbitrating between eating and exercise behaviors?", icon: Stethoscope },
             { text: "How did HON stimulation affect food vs. wheel choice?", icon: Microscope }
+        ],pdf_test2: [
+            { text: "What are the main types of keratinocytic skin cancers and how common are they?", icon: Stethoscope },
+            { text: "What are some key genetic alterations found in basal cell carcinomas?", icon: Microscope },
+            { text: "How Hedgehog signaling pathway led to new targeted therapies for basal cell carcinoma?", icon: Stethoscope },
+            { text: "What role does UV radiation play in the development of cutaneous squamous cell carcinomas?", icon: Microscope }
         ],
+        pdf_test3: [
+            { text: "What was the purpose of establishing the Intergovernmental Working Group?", icon: Stethoscope },
+            { text: "What are some of the key elements included in the global strategy and plan of action?", icon: Microscope },
+            { text: "Why promote research and development was promoted for diseases that disproportionately affect developing countries?", icon: Stethoscope },
+            { text: "What recommendations does the strategy make regarding intellectual property rights and access to medicines?", icon: Microscope }
+        ]
         // ... other model cards
     };
 
